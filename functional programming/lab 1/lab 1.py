a6b23ae7deed5ec0d1c0430be27fcfdd26102217
darya <da6ayar@gmail.com>
2023-10-25 00:25:07 +0300

    edit lab 1

--- functional programming/lab 1/lab 1.py
+++ functional programming/lab 1/lab 1.py
@@ -35,84 +35,86 @@ def sepia(img):
     return ImageOps.colorize(img.convert("L"), "#704238", "#C0B283")
 
 
-def process():
-    if not open_dir:
-        status.config(text='Исходная папка не выбрана', foreground='red')
-    elif not save_dir:
-        status.config(text='Папка для сохранения не выбрана', foreground='red')
-    else:
-        image_paths = [os.path.join(open_dir, filename) for filename in os.listdir(open_dir)
-                       if filename.endswith(('.jpg', '.jpeg', '.png'))]
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.open_dir = None
+        self.save_dir = None
 
-        filters = []
-        if is_resize.get():
-            filters.append(resize)
-        if is_sharp.get():
-            filters.append(sharp)
-        if is_sepia.get():
-            filters.append(sepia)
+        def open_images():
+            self.open_dir = tk.filedialog.askdirectory()
 
-        threads = []
-        for image in image_paths:
-            thread = threading.Thread(target=edit_images, args=(image, status, filters, save_dir))
-            threads.append(thread)
-            thread.start()
+        def save_images():
+            self.save_dir = tk.filedialog.askdirectory()
 
-        for thread in threads:
-            thread.join()
+        def process():
+            if not self.open_dir:
+                status.config(text='Исходная папка не выбрана', foreground='red')
+            elif not self.save_dir:
+                status.config(text='Папка для сохранения не выбрана', foreground='red')
+            else:
+                try:
+                    image_paths = [os.path.join(self.open_dir, filename) for filename in os.listdir(self.open_dir)
+                                   if filename.endswith(('.jpg', '.jpeg', '.png'))]
 
-        status.config(text='Готово!', foreground='green')
+                    filters = []
+                    if is_resize.get():
+                        filters.append(resize)
+                    if is_sharp.get():
+                        filters.append(sharp)
+                    if is_sepia.get():
+                        filters.append(sepia)
+
+                    threads = []
+                    for image in image_paths:
+                        thread = threading.Thread(target=edit_images, args=(image, status, filters, self.save_dir))
+                        threads.append(thread)
+                        thread.start()
+
+                    for thread in threads:
+                        thread.join()
+
+                    status.config(text='Готово!', foreground='green')
+                except:
+                    status.config(text='Ошибка', foreground='red')
+
+        self.title('Обработка изображений')
+        self.geometry("480x220")
+        self.resizable(False, False)
+
+        file_frame = tk.Frame(self, pady=10, padx=10)
+        file_frame.grid(row=0, column=0)
+
+        tk.Label(file_frame, text='Выберите папку с исходными изображениями').grid(row=0, column=0)
+        tk.Button(file_frame, text='Выбрать папку', command=open_images).grid(row=1, column=0)
+        file_frame.rowconfigure(1, minsize=30)
+
+        tk.Label(file_frame, text='''Выберите папку для сохранения
+         обработанных изображений''').grid(row=2, column=0, sticky='s')
+        tk.Button(file_frame, text='Выбрать папку', command=save_images).grid(row=3, column=0)
+        file_frame.rowconfigure(2, minsize=40)
+        file_frame.rowconfigure(3, minsize=30)
+
+        filters_frame = tk.Frame(self, padx=30)
+        filters_frame.grid(row=0, column=1)
+
+        tk.Label(filters_frame, text='Выберите фильтры').grid(row=0, column=0)
+        is_resize = tk.BooleanVar()
+        is_sharp = tk.BooleanVar()
+        is_sepia = tk.BooleanVar()
+        resize_button = tk.Checkbutton(filters_frame, text='Уменьшение размера', variable=is_resize)
+        resize_button.grid(row=1, column=0, sticky='w')
+        sharp_button = tk.Checkbutton(filters_frame, text='Увеличение резкости', variable=is_sharp)
+        sharp_button.grid(row=2, column=0, sticky='w')
+        sepia_button = tk.Checkbutton(filters_frame, text='Сепия', variable=is_sepia)
+        sepia_button.grid(row=3, column=0, sticky='w')
+
+        tk.Button(self, text='Обработать', command=process).grid(row=2, column=0, columnspan=2)
+        status = tk.Label(self, text='')
+        status.grid(row=3, column=0, columnspan=2)
+        self.rowconfigure(3, minsize=45)
 
 
-open_dir = None
-save_dir = None
-
-
-def open_images():
-    global open_dir
-    open_dir = tk.filedialog.askdirectory()
-
-
-def save_images():
-    global save_dir
-    save_dir = tk.filedialog.askdirectory()
-
-
-root = tk.Tk()
-root.title('Обработка изображений')
-root.geometry("480x220")
-root.resizable(False, False)
-
-file_frame = tk.Frame(root, pady=10, padx=10)
-file_frame.grid(row=0, column=0)
-
-tk.Label(file_frame, text='Выберите папку с исходными изображениями').grid(row=0, column=0)
-tk.Button(file_frame, text='Выбрать папку', command=open_images).grid(row=1, column=0)
-file_frame.rowconfigure(1, minsize=30)
-
-tk.Label(file_frame, text='''Выберите папку для сохранения
- обработанных изображений''').grid(row=2, column=0, sticky='s')
-tk.Button(file_frame, text='Выбрать папку', command=save_images).grid(row=3, column=0)
-file_frame.rowconfigure(2, minsize=40)
-file_frame.rowconfigure(3, minsize=30)
-
-filters_frame = tk.Frame(root, padx=30)
-filters_frame.grid(row=0, column=1)
-
-tk.Label(filters_frame, text='Выберите фильтры').grid(row=0, column=0)
-is_resize = tk.BooleanVar()
-is_sharp = tk.BooleanVar()
-is_sepia = tk.BooleanVar()
-resize_button = tk.Checkbutton(filters_frame, text='Уменьшение размера', variable=is_resize)
-resize_button.grid(row=1, column=0, sticky='w')
-sharp_button = tk.Checkbutton(filters_frame, text='Увеличение резкости', variable=is_sharp)
-sharp_button.grid(row=2, column=0, sticky='w')
-sepia_button = tk.Checkbutton(filters_frame, text='Сепия', variable=is_sepia)
-sepia_button.grid(row=3, column=0, sticky='w')
-
-tk.Button(root, text='Обработать', command=process).grid(row=2, column=0, columnspan=2)
-status = tk.Label(root, text='')
-status.grid(row=3, column=0, columnspan=2)
-root.rowconfigure(3, minsize=45)
-
-tk.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
